c4fb7a6adff4b5633f6bd05251ec0ec18c1ab04e
Celindatech <elizabethsalazar.lizarraga@gmail.com>
2025-07-14 22:20:04 -0500

    script.js

--- script.js
+++ script.js
@@ -255,3 +255,26 @@ function verificarEgreso() {
 }
 
 crearMalla();
+function lanzarConfeti() {
+  const duration = 5000;
+  const end = Date.now() + duration;
+
+  (function frame() {
+    confetti({
+      particleCount: 5,
+      angle: 60,
+      spread: 55,
+      origin: { x: 0 }
+    });
+    confetti({
+      particleCount: 5,
+      angle: 120,
+      spread: 55,
+      origin: { x: 1 }
+    });
+
+    if (Date.now() < end) {
+      requestAnimationFrame(frame);
+    }
+  })();
+}
